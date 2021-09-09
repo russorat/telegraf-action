28b6b606f929e7ca5dfd90cb1fdc58b8a3e82e6f
Russ Savage <russ@influxdata.com>
2021-09-09 12:55:59 -0400

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Telegraf Action
+
+This action downloads and installs Telegraf that can be used in your automation.
+
+This downloads the specified version (or `latest` or `nightly`), and adds the `telegraf` command to `/usr/local/bin`. A default configuration is also stored in `/usr/local/etc`.
+
+# Usage
+
+See [action.yml](action.yml)
+
+### Install Telegraf
+```yaml
+steps:
+  - name: Check out repo
+    uses: actions/checkout@v2
+  - name: Install Telegraf
+    uses: influxdata/telegraf-action@v1
+    with:
+      telegraf_version: latest
+```
+
+### Use a specific version of Telegraf
+```yaml
+steps:
+  - name: Check out repo
+    uses: actions/checkout@v2
+  - name: Install Telegraf
+    uses: influxdata/telegraf-action@v1
+    with:
+      influxdb_version: 1.19.0
+```
---
+++ install-telegraf.sh
@@ -0,0 +1,23 @@
+if [ -z "$TELEGRAF_VERSION" ]; then
+  echo 'You need to set telegraf_version. Exiting.'
+  exit 1
+else
+  if [ "$TELEGRAF_VERSION" = "latest" ];
+  then
+      export TELEGRAF_VERSION=$(curl --silent "https://api.github.com/repos/influxdata/telegraf/releases" | jq -r '. | sort_by(.tag_name) | reverse[] | .tag_name | select(test("rc"; "i") | not)' | sed -e 's/v//g' | head -n 1)
+      export TELEGRAF_DOWNLOAD_PATH="https://dl.influxdata.com/telegraf/releases"
+  elif [ "$TELEGRAF_VERSION" = "nightly" ];
+  then
+      export TELEGRAF_DOWNLOAD_PATH="https://dl.influxdata.com/telegraf/nightlies"
+  else
+      export TELEGRAF_DOWNLOAD_PATH="https://dl.influxdata.com/telegraf/releases"
+  fi
+  cd /tmp
+  wget -q $TELEGRAF_DOWNLOAD_PATH/telegraf-${TELEGRAF_VERSION}_linux_amd64.tar.gz
+  tar xvfz telegraf-${TELEGRAF_VERSION}_linux_amd64.tar.gz
+  TELEGRAF_DIR=$(find . -type d -name "telegraf-*" | head -n 1)
+  sudo cp $TELEGRAF_DIR/usr/bin/telegraf /usr/local/bin/
+  sudo cp $TELEGRAF_DIR/etc/* /usr/local/etc/
+  rm -r $TELEGRAF_DIR
+  cd -
+fi
